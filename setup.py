034939e94bba8984b1e47e17cac5f1f9e0cee357
Alejandro Perez <alejandroperezcosio@gmail.com>
2021-12-06 12:05:39 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='ready-python-logs',
-    version='0.2',
+    version='0.3',
     author="Alejandro Perez Cosio",
     author_email="alejandroperezcosio@gmail.com",
     url="https://github.com/alejandroperezcosio/ready-python-logs",
     license='MIT',
     long_description=open('README.md').read(),
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    dependency_links=[
+        'http://github.com/user/repo/tarball/master#egg=rpyconfig'
+    ]
 )
